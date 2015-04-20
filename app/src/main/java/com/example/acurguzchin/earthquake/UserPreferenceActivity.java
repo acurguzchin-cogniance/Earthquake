1be27ac5f410577c8f1d3ea9b8774f19b3d46823
Alexei <alexei.lexx@gmail.com>
2015-04-20 16:33:22 +0300

    fix isValidFragment

--- app/src/main/java/com/example/acurguzchin/earthquake/UserPreferenceActivity.java
+++ app/src/main/java/com/example/acurguzchin/earthquake/UserPreferenceActivity.java
@@ -1,8 +1,10 @@
 package com.example.acurguzchin.earthquake;
 
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by acurguzchin on 06.04.15.
@@ -15,5 +17,10 @@ public class UserPreferenceActivity extends PreferenceActivity {
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return fragmentName.equals("com.example.acurguzchin.earthquake.UserPreferenceFragment");
     }
 }
